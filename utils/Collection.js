279c8a43a694609c2bf9ae022ded276564648912
niczem <nic@sea-watch.org>
2019-02-12 23:09:20 +0100

    collection renamed

--- utils/Collection.js
+++ utils/Collection.js
@@ -5,33 +5,18 @@ function noSpecialChars(str){
  return !/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(str);
 }
 
-var Collection = function(){
+var Collection = function(id){
+  this.id = id;
   this.properties = {};
   this.getPath = function(id){
     const max_depth = 100;
     let path = '';
     var self = this;
-
-    return new Promise(
+    return this.id;
+    /*return new Promise(
       function (resolve, reject) {
-        let callback = function(dir){
-          if(dir === null)
-            reject('could not find directory');
-          path = dir.name+'/'+path;
-          if(dir.parent_directory_id > 0){
-
-            db.Directory.findById(dir.parent_directory_id)
-                .then(callback)
-                .catch(reject)
-          }else{
-            resolve(path);
-          }
-
-        }
-        db.Directory.findById(id)
-                .then(callback)
-                .catch(reject)
-      });
+        
+      });*/
   }
   this.create = function(){
     var self = this;
