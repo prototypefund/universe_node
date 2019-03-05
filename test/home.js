d85bff3bf230e48e3cbf80d87c4793fbe4bed5d7
niczem <nic@sea-watch.org>
2019-03-05 21:18:44 +0100

    first test added

---
+++ test/home.js
@@ -0,0 +1,26 @@
+/**
+ * @name Google search
+ * @desc Searches Google.com for a term and checks if the first link matches. This check should fail.
+ */
+
+const assert = require('assert')
+const puppeteer = require('puppeteer')
+let browser
+let page
+
+before(async () => {
+  browser = await puppeteer.launch()
+  page = await browser.newPage()
+})
+
+describe('Check  Homepage', () => {
+  it('has title "universeOS"', async () => {
+    await page.goto('http://127.0.0.1:8000', { waitUntil: 'networkidle0' })
+    const title = await page.title()
+    assert.equal(title, 'universeOS')
+  }).timeout(10000)
+})
+
+after(async () => {
+  await browser.close()
+})
